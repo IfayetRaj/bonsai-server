3fee868d747cbb79fd78202566f446677a421203
IfayetRaj <a.s.m.rajuddin@gmail.com>
2025-05-24 18:10:52 +0600

    all console deleted

--- index.js
+++ index.js
@@ -44,8 +44,6 @@ async function run() {
     
     app.post('/addplant', async (req, res) =>{
         const newPlant = req.body;
-        console.log(newPlant.userName);
-
         const userResult = await userPlantCollection.insertOne(newPlant);
         res.send(userResult);
     })
@@ -82,7 +80,6 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     
   }
